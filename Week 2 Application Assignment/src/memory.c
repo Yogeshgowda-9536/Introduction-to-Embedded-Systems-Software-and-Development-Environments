65e83d7a26bdeff570687757f02abda6ee863a35
khlaifia Bilel <31816208+KalifiaBillal@users.noreply.github.com>
2020-10-13 00:02:22 +0100

    Update memory.c

--- Week 2 Application Assignment/src/memory.c
+++ Week 2 Application Assignment/src/memory.c
@@ -1,25 +1,3 @@
-/******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
-/**
- * @file memory.c
- * @brief Abstraction of memory read and write operations
- *
- * This implementation file provides an abstraction of reading and
- * writing to memory via function calls. There is also a globally
- * allocated buffer array used for manipulation.
- *
- * @author Alex Fosdick
- * @date April 1 2017
- *
- */
 #include "memory.h"
 
 /***********************************************************
